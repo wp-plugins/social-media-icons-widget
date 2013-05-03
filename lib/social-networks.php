0ef6e38279abacbb677bcde7581d5d37afc2146a
dannisbet <dannisbet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-03 21:38:08 +0000

    Fixed image and css paths
    git-svn-id: https://plugins.svn.wordpress.org/social-media-icons-widget/tags/2013.05@707650 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/social-networks.php
@@ -0,0 +1,27 @@
+<?php
+// Declare supported social media websites
+		global $social_accounts;
+		$social_accounts = array(
+			'Behance' => 'behance',
+			'Dribbble' => 'dribbble',
+			'Facebook' => 'facebook',
+			'Flickr' => 'flickr',
+			'Forrst' => 'forrst',
+			'Foursquare' => 'foursquare',
+			'Google+' => 'googleplus',
+			'Instagram' => 'instagram',
+			'Klout' => 'klout',
+			'LinkedIn' => 'linkedin',
+			'Path' => 'path',
+			'Pinterest' => 'pinterest',
+			'RSS Feed' => 'rss',
+			'StumbleUpon' => 'stumbleupon',
+			'Technorati' => 'technorati',
+			'Tumblr' => 'tumblr',
+			'Twitter' => 'twitter',
+			'Vimeo' => 'vimeo',
+			'Yelp' => 'yelp',
+			'YouTube' => 'youtube',
+			'Zerply' => 'zerply'
+		);
+?>
